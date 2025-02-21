ddf6e26703fe9dd966ca631111166713268dd47e
INUMARTI VEERA VENKATA ESWARA KUMAR <131540117+eswar2005@users.noreply.github.com>
2025-02-21 12:04:29 +0530

    Time: 4 ms (1.93%), Space: 19.1 MB (1.78%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        unordered_map<int ,int > mp;
+        for(int i=0;i<nums.size();i++){
+            mp[nums[i]]++;
+        }
+        for(auto s:mp){
+            if(s.first==target){
+                if(s.second>0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};

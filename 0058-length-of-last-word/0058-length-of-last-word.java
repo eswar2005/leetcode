c6698af3b94b846930d7529c543b291697155525
INUMARTI VEERA VENKATA ESWARA KUMAR <131540117+eswar2005@users.noreply.github.com>
2025-03-08 23:31:30 +0530

    Time: 0 ms (100%), Space: 41.8 MB (47.77%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,35 @@
+class Solution {
+    // public int lengthOfLastWord(String s) {
+    //     int n=s.length();
+    //     s=s.trim();
+    //     int cnt=0;
+    //     for(int i=n-1;i>=0;i--){
+    //         char k = s.charAt(i);
+    //         if(k!=' '){
+    //             cnt++;
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     return cnt;
+
+    // }
+    public int lengthOfLastWord(String s) {
+        int cnt = 0;
+        s = s.trim();
+        int n = s.length();
+        for (int i = n - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            if (ch != ' ') {
+                cnt++;
+            } 
+            else {
+                if (cnt > 0) {
+                    break; 
+                }
+            }
+        }
+        return cnt;
+    }
+}
